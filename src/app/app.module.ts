736b5dc55fdcd01920cae48c2131801afd2397a9
unknown <lazycoder5151@gmail.com>
2018-02-16 10:27:07 -0600

    add the FormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+/* tslint:disable*/
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-
-import { AppComponent } from './app.component';
-import { LogService } from './services/log.service';
-import { LogsComponent } from './logs/logs.component';
-import { LogFormComponent } from './log-form/log-form.component';
+import {FormsModule} from "@angular/forms";
+import {AppComponent} from './app.component';
+import {LogService} from './services/log.service';
+import {LogsComponent} from './logs/logs.component';
+import {LogFormComponent} from './log-form/log-form.component';
 
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LogsComponent,
-    LogFormComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [LogService],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        LogsComponent,
+        LogFormComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule
+    ],
+    providers: [LogService],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
