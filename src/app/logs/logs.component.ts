d0aebd249883ea61bc64f694b9009dd5b9a044e2
unknown <lazycoder5151@gmail.com>
2018-02-16 10:10:26 -0600

    added the onDelete method

--- src/app/logs/logs.component.ts
+++ src/app/logs/logs.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+/* tslint:disable*/
+import {Component, OnInit} from '@angular/core';
+import {Log} from "../models/log";
+import {LogService} from "../services/log.service";
 
 @Component({
-  selector: 'app-logs',
-  templateUrl: './logs.component.html',
-  styleUrls: ['./logs.component.css']
+    selector: 'app-logs',
+    templateUrl: './logs.component.html',
+    styleUrls: ['./logs.component.css']
 })
 export class LogsComponent implements OnInit {
+    logs: Log[];
+    selectedLog: Log;
+    loaded: boolean = false;
 
-  constructor() { }
+    constructor(private logService: LogService) {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.logService.stateClear.subscribe(clear => {
+            if (clear) {
+                this.selectedLog = {id: '', text: '', date: ''}
+            }
+        });
 
+
+        this.logService.getLogs().subscribe((logs) => {
+            this.logs = logs;
+            this.loaded = true;
+        });
+    }
+
+    onSelect(log: Log) {
+        this.logService.setFormLog(log);
+        this.selectedLog = log;
+    }
+
+    onDelete(log: Log) {
+        if (confirm('are you sure?')) {
+            this.logService.deleteLog(log);
+        }
+    }
 }
