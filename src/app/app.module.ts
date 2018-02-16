a02f9ea87555563780dc911cf092772dc05311de
unknown <lazycoder5151@gmail.com>
2018-02-16 10:14:52 -0600

    added the log from to add entries to the dev-log

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { LogService } from './services/log.service';
 import { LogsComponent } from './logs/logs.component';
+import { LogFormComponent } from './log-form/log-form.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LogsComponent
+    LogsComponent,
+    LogFormComponent
   ],
   imports: [
     BrowserModule
